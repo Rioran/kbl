23fb2d93058b18dfdc49b1ab1cb5dd908c80eae2
Rioran <blackdragon71325@gmail.com>
2024-10-08 23:31:48 +0400

    test: add check for kbl file parsing

--- app/parser/parser.py
+++ app/parser/parser.py
@@ -2,11 +2,20 @@ from app.crud import insert_layout
 
 
 def parse_and_load_kbl_file(kbl_path):
-    layout, keys = _parse_kbl_file(kbl_path)
+    layout, keys = parse_kbl_file(kbl_path)
     insert_layout(layout, keys)
 
 
-def _parse_kbl_file(file_path):
+def parse_kbl_file(file_path: str) -> tuple(dict, dict):
+    """Parse content of KBL file into dictionaries.
+
+    Args:
+        file_path (str): full path to KBL file
+
+    Return:
+        layout_data (dict): layout name and optionally description
+        keys_data (dict): map from us_keys to alternative keys
+    """
     layout_data = dict()
     keys_data = dict()
 
--- test/test_parser.py
+++ test/test_parser.py
@@ -1,10 +1,14 @@
-from app.parser.parser import _parse_kbl_file
+from app.parser.parser import parse_kbl_file
 
 from app.config import STATIC_FOLDER
 
 
 def test__parse_kbl_file__russian():
+    expected_layout = {'name': 'RU', 'description': 'Russian classic keyboard'}
+    expected_keys_len = 47
     kbl_path = str((STATIC_FOLDER / 'kbl_files' / 'russian.kbl').resolve())
-    layout, keys = _parse_kbl_file(kbl_path)
-    print(f'{layout = }')
-    print(f'{keys = }')
+
+    layout, keys = parse_kbl_file(kbl_path)
+
+    assert layout == expected_layout, 'layout headers parsed incorrectly'
+    assert len(keys) == expected_keys_len, 'wrong amount of key-pairs'
